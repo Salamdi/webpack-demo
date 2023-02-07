2a9c6aac70ffe97bf0557c448d76f14c2bca2337
Dinislam Abdulsalamov <dinislam.abdulsalamov@lq-0012.local>
2023-02-07 11:08:49 +0300

    add webpack-html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 exports.default = {
   mode: process.env.NODE_ENV || 'development',
   output: {
     clean: true,
     filename: 'bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'index.html' }),
+  ],
 };
